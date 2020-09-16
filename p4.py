3242d2679588864b94e717c44f24262644b03b46
talha <talha@localhost.localdomain>
2020-09-16 13:22:39 +0500

    p4 add

---
+++ p4.py
@@ -0,0 +1,8 @@
+cycle = 100
+bndy = 30
+space = 2
+total_cycle_with_bndy = cycle - bndy
+total_bndy = bndy
+total_spaces = cycle * space
+print("There are " ,cycle, "cycle and " ,bndy, "bndy and each cycle have capicity of " ,space, "people")
+print("Total avl seats are " ,total_spaces, " and total cycle with drivers are " ,total_cycle_with_bndy )
