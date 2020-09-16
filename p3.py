7def44b91e40581f9d6c31f6f69701a0119696ae
talha <talha@localhost.localdomain>
2020-09-16 13:09:40 +0500

    p3 add

---
+++ p3.py
@@ -0,0 +1,6 @@
+print("75% of 4 is ", 75 % 4)
+print("is that true", 6 + 4 < 8)
+print("hens are", 100 - 25 * 3 % 4)
+print("eggs are", 4 + 6 - 5 * 5 / 5)
+print("what is 3 +2?", 3 + 2)
+print("IS that true 4 <= 5", 4 <= 5)
