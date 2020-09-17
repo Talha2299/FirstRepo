9e9ed13f5c539af46fcfd115676a601675239154
talha <talha@localhost.localdomain>
2020-09-17 17:22:39 +0500

    p8 added

---
+++ p8.py
@@ -0,0 +1,9 @@
+formatter = "{} {} {} {}"
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "hey",
+    "how",
+    "are",
+    "you"
+))
