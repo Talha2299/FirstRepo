1a8e768bb1cc2e4e5bbb2cddb6f92269f84446c5
talha <talha@localhost.localdomain>
2020-09-20 19:14:46 +0500

    p22 added

---
+++ p22.py
@@ -0,0 +1,21 @@
+poem = """
+    tThe lovely world
+  with logic so firmly planted
+  cannot discern \n the needs of love
+  nor comprehend passion from intuition
+  and requires an explanation
+  \n\t\twhere there is none.
+   """
+print("_________")
+print(poem)
+print("_________")
+
+def secret(start):
+    jel = start + 10
+    tel = jel * 2
+    cell = tel / 2
+    return cell
+    
+c = secret(4)
+
+print(c)
