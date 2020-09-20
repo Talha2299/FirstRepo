d6c34b26eec98fd2fd2b14f884c917639d898ddb
talha <talha@localhost.localdomain>
2020-09-20 18:51:51 +0500

    p21 added

---
+++ p21.py
@@ -0,0 +1,22 @@
+def add(a, b):
+ print(f"addition {a} + {b}")
+ return a + b
+
+def Sub(a, b):
+ print(f"substraction {a} - {b}")
+ return a - b
+
+def multi(a, b):
+ print(f"multiplicaiotn {a} * {b}")
+ return a * b
+
+def div(a, b):
+ print(f"division {a} / {b}")
+ return a / b
+
+age = add(10, 10)
+height = Sub(100, 50)
+lenght = multi(10, 5)
+width = div(20, 4)
+
+print(f" age:{age}\n height:{height}\n length:{lenght}\n width:{width}")
