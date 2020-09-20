ac5e4663106afbfec36dc5263b529d20c81381bf
talha <talha@localhost.localdomain>
2020-09-20 18:29:52 +0500

    p20 added

---
+++ p20.py
@@ -0,0 +1,14 @@
+def bottles_snacks(bottle, snacks):
+    print(f"you have {bottle} bottles")
+    print(f"you have {snacks} snacks")
+ 
+print("dirct with no")
+bottles_snacks(10, 20)
+
+print("with varibles")
+bb = 30
+ss = 50
+bottles_snacks(bb, ss)
+
+print("add")
+bottles_snacks(bb + 100, ss + 100)
