96ca6f071f0e8a74a77210bd1be2a046b1f16c8d
talha <talha@localhost.localdomain>
2020-09-21 14:44:50 +0500

    p29 added

---
+++ p29.py
@@ -0,0 +1,23 @@
+men = 20
+women = 30
+dogs = 15
+
+if men < women:
+    print("A")
+
+elif men > women:
+    print("B")
+
+else:
+    print("C")
+
+if men < dogs:
+    print("a")
+
+elif men > dogs:
+    print("b")
+
+else:
+    print("c")
+
+
