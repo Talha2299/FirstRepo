957037b36a6036fe0be97fb41c6c40ba88ebacb4
talha <talha@localhost.localdomain>
2020-09-21 15:16:42 +0500

    p31 added

---
+++ p31.py
@@ -0,0 +1,10 @@
+people = [1,2,3,4,5]
+for i in people:
+    print(f"people of type: {i}")
+element = []
+for i in range(0,8):
+    print(f"adding {i}")
+    element.append(i)
+#append is for adding in list
+for i in element:
+    print(f"element {i}")
